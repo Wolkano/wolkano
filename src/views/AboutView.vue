48ed1fa40027983b3b562b2c129ba10eed8bca95
Magnusb99 <magnusb99@hotmail.se>
2024-12-11 13:13:19 +0100

    about us fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="about">
-    <h1>Om Wolkano</h1>
+    <h1>Om <span class="cName">Wolkano</span></h1>
     <div class="abInfo">
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    </p>
-    <img src="../assets/it-guys.jpeg" alt="4-IT-killar"/>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.    
+    </p>
+      <img src="../assets/it-guys.jpeg" alt="4-IT-killar"/>
+  </div>
+  <div class="kButton">
+  <router-link to="/test" class="button">Kontakta oss</router-link>
   </div>
   </div>
 </template>
 <script setup></script>
 <style lang="scss" scoped>
-
+.cName{
+      color: #fe9d01;
+      text-shadow: 0px 1px 1px black;
+    }
 .about{
   display: flex;
   flex-wrap: wrap;
@@ -25,13 +32,15 @@
     font-size: 48px;
     font-weight: bold;
     width: 100%;
+    
   }
+  
 
   .abInfo{
     display: flex;
     flex-direction: row;
     justify-content: center;
-    height: auto;
+    gap: 100px;
     width: 100%;
 
     p{
@@ -43,6 +52,17 @@
     img{
       border-radius: 3px;
       width: 300px;
+    }
+  }
+
+  .kButton{
+    background-color: white;
+    border-radius: 100px;
+    color: black;
+    padding: 10px 20px;
+    transition: background-color 200ms linear;
+    &:hover {
+      background-color: rgb(231, 231, 231);
     }
   }
 
