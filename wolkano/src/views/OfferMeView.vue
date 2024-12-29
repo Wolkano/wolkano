8410b19915c429a494ae9790de0c45115daf2204
Magnusb99 <magnusb99@hotmail.se>
2024-12-29 14:18:54 +0100

    commitmange

--- wolkano/src/views/OfferMeView.vue
+++ wolkano/src/views/OfferMeView.vue
@@ -6,22 +6,18 @@
       </div>
       <div class="col">
         <div class="animation">
-          <img src="../assets/testProfilePicture.png" />
+          <img src="../assets/Unknown.jpg" />
         </div>
         <div class="section">
-          <h2>Heading 2</h2>
+          <h2>Offerter</h2>
           <p class="infoText">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            OfferMe är tjänsten som revolutionerar hur offerter skapas. Med vår smarta och användarvänliga plattform kan kunder snabbt och enkelt få en skräddarsydd offert direkt efter att de har fyllt i alla nödvändiga uppgifter.
           </p>
         </div>
       </div>
       <div class="col">
+        <div class="section">
+          <h2>Heading 2</h2>
         <p class="infoText">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -31,14 +27,17 @@
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        </div>
         <div class="animation">
-          <img src="../assets/testProfilePicture.png" />
+          <img src="../assets/Förnamn.gif" />
         </div>
       </div>
       <div class="col">
         <div class="animation">
           <img src="../assets/testProfilePicture.png" />
         </div>
+        <div class="section">
+          <h2>Heading 2</h2>
         <p class="infoText">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -48,6 +47,7 @@
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        </div>
       </div>
     </div>
 
@@ -91,14 +91,35 @@ import CompanyBanner from "@/components/CompanyBanner.vue";
     justify-content: center;
     align-items: center;
 
-    p {
-      font-size: $font-size-p;
-      width: 35%;
-      padding: 50px;
-    }
+
     .animation {
       width: 35%;
-      padding: 100px;
+      margin: 30px;
+      padding: 30px;
+
+      img{
+      border-style: solid;
+      border-width: 5px;
+      border-color: #fe9d01;
+      border-radius: 20px;
+      }
+
+    }
+    .section{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 35%;
+
+      p {
+      font-size: $font-size-p;
+      width: 70%;
+    }
+    h2{
+      font-size: $font-size-h2;
+      color: #fe9d01;
+      font-weight: 700;
+    }
     }
   }
   .col:nth-of-type(3) {
