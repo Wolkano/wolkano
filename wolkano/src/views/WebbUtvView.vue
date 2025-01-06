9ad97c903ab9e95351d197b9b335ca6004a9962e
Magnusb99 <magnusb99@hotmail.se>
2025-01-06 17:16:43 +0100

    fixa classnamn på webbutv sidan

--- wolkano/src/views/WebbUtvView.vue
+++ wolkano/src/views/WebbUtvView.vue
@@ -3,8 +3,13 @@
     <div class="titel">
       <h1>Webbutveckling</h1>
     </div>
-    <div class="infoDiv" >
-      <img src="../assets/placeholderbild.jpg" v-motion-pop-visible-once :duration="700" :delay="50"/>
+    <div class="body">
+      <img
+        src="../assets/placeholderbild.jpg"
+        v-motion-pop-visible-once
+        :duration="700"
+        :delay="50"
+      />
       <div class="info" v-motion-pop-visible-once :duration="700" :delay="100">
         <h2>Hemsidor som passar dina behov</h2>
         <p>
@@ -18,15 +23,12 @@
           förverkliga dem!
         </p>
         <div class="kontakta">
-        <p>Behöver du en hemsida? Tveka inte att kontakta oss</p>
-        <div class="button">
-          
-          <router-link to="/form" class="button">Kontakta oss</router-link>
-
+          <p>Behöver du en hemsida? Tveka inte att kontakta oss</p>
+          <div class="button">
+            <router-link to="/form" class="button">Kontakta oss</router-link>
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
-    
     </div>
   </div>
 </template>
@@ -57,7 +59,7 @@
       border-bottom: 2px solid #fe9d01;
     }
   }
-  .infoDiv {
+  .body {
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -66,7 +68,6 @@
     img {
       width: 35%;
       border-radius: 5px;
-
     }
     .info {
       display: flex;
@@ -80,14 +81,14 @@
         width: 70%;
         font-size: #{$font-size-p};
       }
-      .kontakta{
+      .kontakta {
         width: 100%;
         display: flex;
         flex-direction: column;
         align-items: center;
         gap: 20px;
 
-        p{
+        p {
           border-top: 2px solid white;
           padding-top: 10px;
           margin-top: 10px;
@@ -99,48 +100,43 @@
       }
     }
   }
+}
 
-
-  }
-
-@media (max-width: 768px){
-  .webbUtv{
-
-    .titel{
-    h1{
-      font-size: $font-size-mobile-h1;
+@media (max-width: 768px) {
+  .webbUtv {
+    .titel {
+      h1 {
+        font-size: $font-size-mobile-h1;
+      }
     }
-  }
-    .infoDiv{
+    .infoDiv {
       flex-direction: column-reverse;
       justify-content: center;
       gap: 50px;
 
-      img{
+      img {
         width: 100%;
         padding-left: 20px;
         padding-right: 20px;
-        }
+      }
 
-      .info{
+      .info {
         width: 100%;
         padding-left: 20px;
         padding-right: 20px;
-        
-        h2{
+
+        h2 {
           width: 100%;
           font-size: $font-size-mobile-h3;
           text-align: left;
           font-weight: 600;
         }
-        p{
+        p {
           width: 100%;
           text-align: left;
         }
-        
       }
     }
-
   }
 }
 </style>
