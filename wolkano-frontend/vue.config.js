68201a8b12f546116316bbf84414f6869644c71e
Gabriel Holmkvist <gabbeholman99@gmail.com>
2025-02-10 19:01:23 +0100

    Added prerender

--- wolkano-frontend/vue.config.js
+++ wolkano-frontend/vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.plugins.push(
+        new PrerenderSPAPlugin({
+          staticDir: path.resolve(__dirname, "dist"),
+          routes: ["/form", "/#/form", "/"], // Add more routes if needed
+        })
+      );
+    }
+  },
+});
