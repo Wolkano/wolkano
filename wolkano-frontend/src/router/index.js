9896fdb9edb2f06344b8baec21745af9c485281f
Gabriel Holmkvist <gabbeholman99@gmail.com>
2025-02-10 19:30:46 +0100

    fix for netlify forms, stand in skeleton form hidden

--- wolkano-frontend/src/router/index.js
+++ wolkano-frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import FormView from "../views/FormView.vue";
 import AboutView from "../views/AboutView.vue";
@@ -42,7 +42,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 };
--- wolkano-frontend/vue.config.js
+++ wolkano-frontend/vue.config.js
@@ -1,17 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-const path = require("path");
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: (config) => {
-    if (process.env.NODE_ENV === "production") {
-      config.plugins.push(
-        new PrerenderSPAPlugin({
-          staticDir: path.resolve(__dirname, "dist"),
-          routes: ["/form", "/#/form", "/"], // Add more routes if needed
-        })
-      );
-    }
-  },
 });
