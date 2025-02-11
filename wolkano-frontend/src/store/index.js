fdc876b8bc787c594a8fb23a3bb6965a2d417ed3
Gabriel Holmkvist <gabbeholman99@gmail.com>
2025-02-11 18:31:12 +0100

    Working email function

--- wolkano-frontend/src/store/index.js
+++ wolkano-frontend/src/store/index.js
@@ -1,6 +1,11 @@
 import { createStore } from "vuex";
 import dictionary from "./dictionary.json";
 import axios from "axios";
+import emailjs from "@emailjs/browser";
+
+const YOUR_SERVICE_ID = "service_0nygmim";
+const YOUR_TEMPLATE_ID = "template_qpul5cj";
+const PUBLIC_KEY = "jrV_u3V8n1T9JLxFA";
 
 export default createStore({
   state: {
@@ -27,20 +32,21 @@ export default createStore({
     },
   },
   actions: {
-    async submitToNotion({ commit }, customerInfo) {
-      (async () => {
-        try {
-          await axios.post(
-            "http://localhost:3000/api/send-to-zapier",
-            customerInfo
-          );
-          commit("setSubmittedSuccessfully", true);
-          commit("setHasSubmitted", true);
-        } catch (error) {
-          commit("setSubmittedSuccessfully", false);
-          commit("setHasSubmitted", true);
-        }
-      })();
+    sendEmail({ commit }, userInfo) {
+      emailjs.init(PUBLIC_KEY);
+      try {
+        emailjs.send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, {
+          from_name: userInfo.name,
+          from_email: userInfo.email,
+          message: userInfo.info,
+          company: userInfo.company,
+          phone: userInfo.phone,
+        });
+        commit("setSubmittedSuccessfully", true);
+        commit("setHasSubmitted", true);
+      } catch (error) {
+        console.log({ error });
+      }
     },
     async submitOffer({ commit }, offerInfo) {
       (async () => {
